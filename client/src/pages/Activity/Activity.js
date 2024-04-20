ee7b40f589a2e47c207cab8898f4803bd8812dbc
Luis Feliz <Luisfeliz@gmail.com>
2024-04-20 15:16:10 -0400

    updates_to_publish_v1.0.5

--- client/src/pages/Activity/Activity.js
+++ client/src/pages/Activity/Activity.js
@@ -2,8 +2,8 @@ import React from "react";
 import RecentActivity from "../../components/RecentActivity/RecentActivity";
 import LeftNav from "../../components/LeftNav/LeftNav";
 import ToolTip from "../../components/ToolTips";
-// import { MdCalendarMonth } from "react-icons/md";
-// import { GrSplits } from "react-icons/gr";
+import { MdCalendarMonth } from "react-icons/md";
+import { GrSplits } from "react-icons/gr";
 
 import "./activityStyle.css";
 
@@ -52,14 +52,14 @@ function Activity() {
         </div>
 
         <div className="actvity-card-split">
-        {/* <div className="split-icon"><GrSplits /></div> */}
+        <div className="split-icon"><GrSplits /></div>
           <span className="split-label">Split up your Spending into monthly payments no extra charges or interest fees.</span>
           <span className="split-terms">TERMS & CONDITIONS</span>
         </div>
 
 
         <div className="actvity-dl-statement">
-       {/* <div className="statement-icon"><MdCalendarMonth /> </div> */}
+       <div className="statement-icon"><MdCalendarMonth /> </div>
        <span className="statement-label">  Download Your Monthly Statements</span>
         </div>
 
