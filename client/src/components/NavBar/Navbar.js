27e3bc57dd82a33163541f2f078e406788e82399
Luis Feliz <Luisfeliz@gmail.com>
2024-04-28 22:33:12 -0400

    responsive_v1.0.5

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -3,13 +3,32 @@ import "./style.css";
 
 function Nav() {
 	return (
-    <nav className="navbar navbar-expand-lg bg-light">
+<div>
+
+
+    <nav  className="nav-container">
+
+      <div className="nav-box">
+
+      <a className="nav-title" href="/dashboard">Payment Center</a>
+      <a className="nav-link active box"  href="/activity">Activity</a>
+      <a className="nav-link box link-2" href="#accountservices">Account Services</a>
+      <a className="nav-link box help" href="#accountservices">Help</a>
+      <a className="btn btn-outline-primary nav-box-button" href="/login" type="submit">Log In</a>
+      </div>
+    </nav>
+
+
+{/* 
+
+<nav className="navbar main navbar-expand-lg bg-light">
     <div className="container-fluid">
+
+      <div className="collapse navbar-collapse nav-link-container" id="navbarSupportedContent">
       <a className="navbar-brand" href="/dashboard">Payment Center</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="/activity">Activity</a>
@@ -21,8 +40,15 @@ function Nav() {
         <a className="nav-link help" href="#accountservices">Help</a>
           <a className="btn btn-outline-primary" href="/login" type="submit">Log In</a>
       </div>
+
+
+      
     </div>
-  </nav>
+  </nav> */}
+
+
+  
+</div>
 	);
 }
 
