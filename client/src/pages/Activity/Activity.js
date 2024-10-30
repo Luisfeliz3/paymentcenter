c0db0e724f852610d447fddf1a7cbea94d2cbc2c
Luis Feliz <Luisfeliz@gmail.com>
2024-10-29 21:52:09 -0400

    activity page design-update

--- client/src/pages/Activity/Activity.js
+++ client/src/pages/Activity/Activity.js
@@ -9,49 +9,52 @@ import "./activityStyle.css";
 
 function Activity() {
   return (
-    <div>
+   <div>
       <LeftNav className="sidesbar" />
-
+     <div className="container">
+  
       <div className="activity-container">
         <div className="activity">
-          <div className="actvity-card-group">
-            <div className="actvity-card vital">
-              <div className="actvity-card-body">
-                <h5 className="actvity-card-title">
+          <div className="activity-card-group">
+            <div className="activity-card vital">
+              <div className="activity-card-body">
+                <h5 className="activity-card-title">
                   <div className="charges-tooltip">
               
                   </div>
-                  Pendin Charges
+                  Pending Charges
                 </h5>
-                <p className="actvity-card-text">$400.00</p>
+                <p className="activity-card-text">$400.00</p>
               </div>
             </div>
-            <div className="actvity-card vital">
-              <div className="actvity-card-body">
-                <h5 className="actvity-card-title">
+            <div className="activity-card vital">
+              <div className="activity-card-body">
+                <h5 className="activity-card-title">
                   <div className="charges-tooltip">
               
                   </div>
                   Posted Charges
                 </h5>
-                <p className="actvity-card-text">$40.00</p>
+                <p className="activity-card-text">$40.00</p>
               </div>
             </div>
-            <div className="actvity-card vital">
-              <div className="actvity-card-body">
-                <h5 className="actvity-card-title">
+            <div className="activity-card vital">
+              <div className="activity-card-body">
+                <h5 className="activity-card-title">
                   <div className="charges-tooltip">
             
                   </div>
                   Total Balance
                 </h5>
-                <p className="actvity-card-text">$834.55</p>
+                <p className="activity-card-text">$834.55</p>
               </div>
             </div>
           </div>
         </div>
-<div className="actvity-card-spend"></div>
-        <div className="actvity-card-split">
+{/* <div className="activity-card-spend">
+
+</div> */}
+        <div className="activity-card-split">
         
           <span className="split-label">Split up your Spending into monthly payments no extra charges or interest fees.</span>
           <span className="split-terms">TERMS & CONDITIONS</span>
@@ -59,18 +62,19 @@ function Activity() {
         </div>
 
 
-        <div className="actvity-dl-statement">
+        <div className="activity-dl-statement">
        <div className="statement-icon"><MdCalendarMonth /> </div>
        <span className="statement-label">  Download Your Monthly Statements</span>
         </div>
 
 
-        <div className="actvity-card-spend"></div>
+        <div className="activity-card-spend"></div>
         <section id="recent-activity">
           <RecentActivity width={735} />
         </section>
       </div>
     </div>
+   </div>
   );
 }
 
